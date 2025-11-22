96048d2e4f85353a53946f86ce039e8b2e57519e
Damian Robert Tule Skeen <damianskeen@u.boisestate.edu>
2025-11-22 01:48:24 +0000

    Some better formatting

--- routes/index.js
+++ routes/index.js
@@ -4,93 +4,115 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next){
   try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
+    req.db.query('SELECT * FROM todos;', (err, results) => 
+      {
+      if (err) 
+        {
         console.error('Error fetching todos:', err);
         return res.status(500).send('Error fetching todos');
-      }
+        }
       res.render('index', { title: 'My Simple TODO', todos: results });
     });
-  } catch (error) {
+  } catch (error) 
+  {
     console.error('Error fetching items:', error);
     res.status(500).send('Error fetching items');
   }
 });
 
-router.post('/create', function (req, res, next) {
+router.post('/create', function (req, res, next) 
+{
     const { task } = req.body;
     
     //validation for blank tasks
-    if (!task || task.trim() === '') {
+    if (!task || task.trim() === '') 
+      {
       return res.status(400).send('Task cannot be empty.');
-    }
+      }
 
     try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
-        if (err) {
+      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => 
+        {
+        if (err) 
+          {
           console.error('Error adding todo:', err);
           return res.status(500).send('Error adding todo');
-        }
+          }
         console.log('Todo added successfully:', results);
         // Redirect to the home page after adding
         res.redirect('/');
       });
-    } catch (error) {
+    } catch (error) 
+    {
       console.error('Error adding todo:', error);
       res.status(500).send('Error adding todo');
     }
 });
 
-router.post('/delete', function (req, res, next) {
+router.post('/delete', function (req, res, next) 
+{
     const { id } = req.body;
     try {
-      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => {
-        if (err) {
+      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => 
+        {
+        if (err) 
+          {
           console.error('Error deleting todo:', err);
           return res.status(500).send('Error deleting todo');
-        }
+          }
         console.log('Todo deleted successfully:', results);
         // Redirect to the home page after deletion
         res.redirect('/');
     });
-    }catch (error) {
+    }catch (error) 
+    {
         console.error('Error deleting todo:', error);
         res.status(500).send('Error deleting todo:');
     }
 });
 
 // update a task
-router.post('/edit', function (req, res, next) {
+router.post('/edit', function (req, res, next) 
+{
     const { id, task } = req.body;
-    if (!task || task.trim() === '') {
+    
+    if (!task || task.trim() === '') 
+      {
         return res.status(400).send('Task cannot be empty.');
-    }
+      }
     try {
-        req.db.query('UPDATE todos SET task = ? WHERE id = ?;', [task, id], (err, results) => {
-            if (err) {
+        req.db.query('UPDATE todos SET task = ? WHERE id = ?;', [task, id], (err, results) => 
+          {
+            if (err) 
+              {
                 console.error('Error updating todo:', err);
                 return res.status(500).send('Error updating todo');
-            }
+              }
             res.redirect('/');
         });
-    } catch (error) {
+    } catch (error) 
+    {
         console.error('Error updating todo:', error);
         res.status(500).send('Error updating todo');
     }
 });
 
 //mark as completed
-router.post('/complete', function (req, res, next) {
+router.post('/complete', function (req, res, next) 
+{
     const { id } = req.body;
     try {
-        req.db.query('UPDATE todos SET completed = TRUE WHERE id = ?;', [id], (err, results) => {
-            if (err) {
+        req.db.query('UPDATE todos SET completed = TRUE WHERE id = ?;', [id], (err, results) => 
+          {
+            if (err) 
+              {
                 console.error('Error completing todo:', err);
                 return res.status(500).send('Error completing todo');
-            }
+              }
             res.redirect('/');
         });
-    } catch (error) {
+    } catch (error) 
+    {
         console.error('Error completing todo:', error);
         res.status(500).send('Error completing todo');
     }
